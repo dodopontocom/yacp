9f6002644f05c9259c705a72a053a72efc9517b3
Rodolfo Neto <rodolfotiago@gmail.com>
2022-11-01 11:10:07 -0300

    Update and rename telegram/yacp-liveview.sh to telegram/darlene-bot/yacp-liveview.sh

--- telegram/darlene-bot/yacp-liveview.sh
+++ telegram/darlene-bot/yacp-liveview.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 export BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
+source ${BASEDIR}/../.env
 
 _VERSION="0.1 (beta)"
-
-source ${BASEDIR}/.env
 
 ######script helper functions ####################################################
 # check if required variables are exported
