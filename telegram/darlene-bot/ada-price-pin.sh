9c496986f229a51a872d0429a01bac0db6d26ce9
dodopontocom <rodolfotiago@gmail.com>
2022-11-01 17:13:31 -0300

    wip: version 2 of ada price

--- telegram/darlene-bot/ada-price-pin.sh
+++ telegram/darlene-bot/ada-price-pin.sh
@@ -18,40 +18,23 @@ helper.validate_vars() {
     fi
   done
 }
-
-# curl command to get metrics
-helper._curlgrep(){
-	curl -sS localhost:12798/metrics | grep ${1} | cut -d' ' -f2	
-}
 ###################################################################################
 
-helper.validate_vars TELEGRAM_BOT_TOKEN TELEGRAM_CHAT_ID
+helper.validate_vars TELEGRAM_BOT_TOKEN TELEGRAM_CHAT_ID SCRIPT_CONF
+
+[[ -f ${SCRIPT_CONF} ]] || touch ${SCRIPT_CONF}
 
 binance_api="https://api.binance.com/api/v3/ticker/price?symbol=ADAUSDT"
 price=$(curl -sS ${binance_api} | jq -r '.price')
 
-CHANNEL_MESSAGE_ID="17"
 _STRONG="<b>"
 STRONG_="</b>"
 _ITALIC="<i>"
 ITALIC_="</i>"
-_CODE="<pre>"
-CODE_="</pre>"
-_SPOILER="<tg-spoiler>"
-SPOILER_="</tg-spoiler>"
 _NEWLINE="%0A"
 _BOT="🤖"
-_SPARKLING="✨"
-_BR="🇧🇷" # br flag
-_INFI="♾"
-_TX="📝"
-_OK="✅"
-_NOK="‼️"
-_BLOCK="⛏️"
-_BATTLE="⚔️"
 _FIRE="🔥"
 _INCREASE="📈"
-_ROCKET="🚀"
 
 message="
 ${_STRONG}| ${_INCREASE} USD \$${price} | LIVE ADA${STRONG_}
@@ -63,14 +46,21 @@ ${_NEWLINE}
 Cool Delegators give it a fire ${_FIRE}
 "
 
-# send dump message
-# get bot updates
-# get message_id from this dump message
-# update MESSAGE_ID var
+if [[ "$(cat ${SCRIPT_CONF})" -eq "" ]]; then
+  _pholder="$(date +%s)"
+  CHANNEL_MESSAGE_ID=$(curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage \
+  	-d chat_id=${TELEGRAM_CHAT_ID} \
+	  -d text="${_pholder}" \
+	  -d parse_mode=HTML | jq -r '.result.message_id')
+    echo "${CHANNEL_MESSAGE_ID}" > ${SCRIPT_CONF}
+
+    else
+      CHANNEL_MESSAGE_ID=$(cat ${SCRIPT_CONF})
+fi
 
 #send to channel/group/direct
-curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/editMessageText \
-	-d chat_id=${TELEGRAM_CHAT_ID} \
+curl -sS -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/editMessageText \
+ -d chat_id=${TELEGRAM_CHAT_ID} \
 	-d text="${message}" \
 	-d parse_mode=HTML \
-	-d message_id=${CHANNEL_MESSAGE_ID}
+	-d message_id=${CHANNEL_MESSAGE_ID} > /dev/null
