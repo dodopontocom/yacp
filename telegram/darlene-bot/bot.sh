7b46dc7c3a8204ef59a8dbccc94acae3b9f9ea61
dodopontocom <rodolfotiago@gmail.com>
2022-11-03 15:49:27 -0300

    wip: bot alpha

--- telegram/darlene-bot/bot.sh
+++ telegram/darlene-bot/bot.sh
@@ -20,28 +20,55 @@ helper.get_api
 source ${BASEDIR}/ShellBot.sh
 ShellBot.init --token ${TELEGRAM_BOT_TOKEN} --monitor --flush
 
+_ITALIC="<i>"
+ITALIC_="</i>"
+
 while :
 do
 	ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
-  #my_chat_member_chat_type "group\|supergroup\|channel"
-  #my_chat_member_chat_id
-  #my_chat_member_chat_title
-  #my_chat_member_new_chat_member_status "member\|left\|kicked\|administrator"
-  #my_chat_member_new_chat_member_status
 
   if [[ ! -z ${my_chat_member_new_chat_member_status[$id]} ]]; then
     case ${my_chat_member_new_chat_member_status[$id]} in
-      left|kicked) echo "left" ;;
+      left|kicked) bot.left ;;
       "member") bot.new_member ;;
-      "administrator") echo "administrator" ;;
+      "administrator") bot.is_amdin ;;
     esac
   fi
 
-  #ShellBot.editMessageText \
-  #      --chat_id ${TESTING_CHAT_ID} \
-  #      --message_id "18" \
-  #      --text "<tg-spoiler>    rodolfo tiago de $(date +%s)    </tg-spoiler>" \
-  #      --parse_mode html
+  ticker="ADAUSDT"
+  price="USD \$$(curl -sS ${BINANCE_API}${ticker} | jq -r '.price')"
+  message="
+  📈 ADA -> ${price}
+  \n
+  -= LIVE ADA PRICE =-
+  \n\n\n
+  ${args[@]-_____________________________}
+  \n
+  ${_ITALIC}$(date +"%t%d %b %Y %H:%M:%S")${ITALIC_}
+  "
+
+  message_gone=($(cat ${SCRIPT_CONF} | grep "left" | cut -d'|' -f5))
+  if [[ ! -z ${message_gone} ]]; then
+    for g in $(echo ${message_gone[@]}); do
+      for i in $(cat ${SCRIPT_CONF} | grep -v "left" | cut -d'|' -f1,2); do
+        if [[ "$(echo ${i} | cut -d'|' -f1)" != "$(echo ${g})" ]]; then
+          ShellBot.editMessageText \
+            --chat_id "$(echo ${i} | cut -d'|' -f2)" \
+            --message_id "$(echo ${i} | cut -d'|' -f1)" \
+            --text "$(echo -e ${message})" \
+            --parse_mode html
+        fi
+      done
+    done
+  else
+    for i in $(cat ${SCRIPT_CONF} | grep -v "left" | cut -d'|' -f1,2); do
+      ShellBot.editMessageText \
+        --chat_id "$(echo ${i} | cut -d'|' -f2)" \
+        --message_id "$(echo ${i} | cut -d'|' -f1)" \
+        --text "$(echo -e ${message})" \
+        --parse_mode html
+    done
+  fi
 
 	for id in $(ShellBot.ListUpdates)
 	do
--- telegram/darlene-bot/f/bot-command.sh
+++ telegram/darlene-bot/f/bot-command.sh
@@ -2,44 +2,104 @@
 
 # Init commands
 
+_ITALIC="<i>"
+ITALIC_="</i>"
+
 bot.init() {
   local args=($@)
 
   echo "init --- ${args[@]}"
 
   case ${args[0]} in
-    "/ada") bot.start ${args[@]:1};;
+    "/ada") bot.ada ${args[@]:1};;
   esac
   
 }
 
-bot.start() {
-  local args=($@)
+bot.ada() {
+  local args=($@) message ticker price
 
-  echo "ada --- ${args[@]}"
+  ticker="ADAUSDT"
+  price="USD \$$(curl -sS ${BINANCE_API}${ticker} | jq -r '.price')"
 
-  [[ -z ${args[@]} ]] && args="0"
+  ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
+  _is_permitted=$?
 
-  if [[ $(cat ${SCRIPT_CONF} | grep -E "${message_chat_id[$id]}") ]]; then
-    ShellBot.editMessageText \
-      --chat_id "${message_chat_id[$id]}" \
-      --message_id "$(cat ${SCRIPT_CONF} | grep -E "${message_chat_id[$id]}" | cut -d'|' -f1)" \
-      --text "rodolfo tiago de $(date +%s)" \
+  message="
+  📈 ADA -> ${price}
+  \n
+  -= LIVE ADA PRICE =-
+  \n\n\n
+  ${args[@]-_____________________________}
+  \n
+  ${_ITALIC}$(date +"%t%d %b %Y %H:%M:%S")${ITALIC_}
+  "
+
+  if [[ ${message_chat_type[$id]} != "private" ]] && \
+    [[ ${_is_permitted} == "0" ]]; then
+    if [[ $(cat ${SCRIPT_CONF} | grep -E -- "${message_chat_id[$id]}" | tail -1 | grep -E -- "${message_chat_id[$id]}\|ada") ]]; then
+      ShellBot.editMessageText \
+        --chat_id "${message_chat_id[$id]}" \
+        --message_id "$(cat ${SCRIPT_CONF} | grep -E -- "${message_chat_id[$id]}" | tail -1 | cut -d'|' -f1)" \
+        --text "$(echo -e ${message})" \
+        --parse_mode html
+    else
+      ShellBot.sendMessage \
+        --chat_id ${message_chat_id[$id]} \
+        --text "$(echo -e ${message})" \
+        --parse_mode html | cut -d'|' -f2,7 \
+        | xargs -I {} echo "{}|ada|$(date +%Y%mm%dd%Hh%Mm)" >> ${SCRIPT_CONF}
+
+      ShellBot.pinChatMessage \
+        --chat_id "${message_chat_id[$id]}" \
+        --message_id "$(tail -1 ${SCRIPT_CONF} | cut -d'|' -f1)"
+    fi
+  
+  elif [[ ${message_chat_type[$id]} == "private" ]]; then
+    message="Sorry, this command works only in groups/channels"
+    ShellBot.sendMessage \
+      --chat_id ${message_from_id[$id]} \
+      --text "$(echo -e ${message})" \
       --parse_mode html
-  else
+  
+  elif [[ ${_is_permitted} != "0" ]]; then
+    message="I still not permitted to perform this task\n"
+    message+="Please, ask the Admins to promote me as an Admin to this group"
     ShellBot.sendMessage \
       --chat_id ${message_chat_id[$id]} \
-      --text "$(echo -e ${args[@]})" \
-      --parse_mode html | cut -d'|' -f2,7 | tee ${SCRIPT_CONF} 
+      --text "$(echo -e ${message})" \
+      --parse_mode html
+
   fi
+}
+
+bot.left() {
+  local message get_mess
+  get_mess="$(cat ${SCRIPT_CONF} | grep -E -- "${my_chat_member_chat_id[$id]}" | grep -v "left" | tail -1 | cut -d'|' -f1)"
+  message="Be aware bot has left the group \`\"${my_chat_member_chat_title[$id]}\"\`"
+  ShellBot.sendMessage \
+    --chat_id ${my_chat_member_from_id[$id]} \
+    --text "$(echo -e ${message})" \
+    --parse_mode markdown | cut -d'|' -f2,7 \
+    | xargs -I {} echo "{}|${my_chat_member_chat_id[$id]}|left|${get_mess}|$(date +%Y%mm%dd%Hh%Mm)" >> ${SCRIPT_CONF}
 }
 
 bot.new_member() {
   local message
 
-  echo "--- ${my_chat_member_from_id[$id]}"
-  message="I was added to the group \`\"${my_chat_member_chat_title[$id]}\"\` \n"
-  message+="Please, now promote me to admin"
+  message="I am now added to \`\"${my_chat_member_chat_title[$id]}\"\` group\n"
+  message+="Do not forget to promote me as Admin"
+
+  ShellBot.sendMessage \
+    --chat_id ${my_chat_member_from_id[$id]} \
+    --text "$(echo -e ${message})" \
+    --parse_mode markdown
+}
+
+bot.is_amdin() {
+  local message
+
+  message="I am now promoted to admin on \`\"${my_chat_member_chat_title[$id]}\"\` group\n"
 
   ShellBot.sendMessage \
     --chat_id ${my_chat_member_from_id[$id]} \
