0f3bf6bc58336afbffa8f8e0ab17448806788598
dodopontocom <rodolfotiago@gmail.com>
2022-11-11 15:56:38 -0300

    wip: improvments

--- telegram/bot/yacp-epoch-transition.sh
+++ telegram/bot/yacp-epoch-transition.sh
@@ -52,11 +52,12 @@ if [[ $(helper._curlgrep ${_metric_slot_in_epoch}) -ge 300 ]]; then
       touch /tmp/30min
     fi
   fi
-  if [[ ! -f /tmp/3h ]]; then
-    if [[ $(($(date "+%s") + (${EPOCH_IN_SECONDS} - $(helper._curlgrep ${_metric_slot_in_epoch})))) -le $(($(date "+%s") + 10800)) ]]; then
+  if [[ ! -f /tmp/1h ]]; then
+    #test ---> 2.5 hour
+    if [[ $(($(date "+%s") + (${EPOCH_IN_SECONDS} - $(helper._curlgrep ${_metric_slot_in_epoch})))) -le $(($(date "+%s") + 9000)) ]]; then
     #if [[ $(($(date "+%s") + (${EPOCH_IN_SECONDS} - $(helper._curlgrep ${_metric_slot_in_epoch})))) -le $(($(date "+%s") + 3600)) ]]; then
       message="less than 3 hours to epoch transition"
-      touch /tmp/3h
+      touch /tmp/1h
 
       #send monitoring to telegram
       /home/ubuntu/yacp/telegram/bot/f/_gnuplot.sh >> /tmp/plot
@@ -72,16 +73,32 @@ if [[ $(helper._curlgrep ${_metric_slot_in_epoch}) -ge 300 ]]; then
 
   else
     #epoch just started
-    rm /tmp/1min /tmp/5min /tmp/30min /tmp/5h /tmp/3h /tmp/far
-    message="new Epoch ($(helper._curlgrep ${_metric_epoch})) just started"
+
+    rm /tmp/1min /tmp/5min /tmp/30min /tmp/5h /tmp/1h /tmp/far
+
+    _message="new Epoch ($(helper._curlgrep ${_metric_epoch})) just started"
     curl -s -X POST https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage \
 	    -d chat_id=${TESTING_CHAT_ID} \
-	    -d text="${message}" \
+	    -d text="${_message}" \
 	    -d parse_mode=HTML
 
+    #execute monitoring
+    df -k /dev/sda1 | awk '{print $5}' | egrep "[0-9]" | cut -c-2 | xargs -I {} echo "$(date -d @$(date +\%s) +\%Y-\%m-\%d-\%H:\%M:\%S) {}" >> /home/ubuntu/.yacp-disk
+    du -sh /home/ubuntu/cardano-node/db/ | awk '{print $1}' | grep "[0-9]" | tr 'G' '\n' | xargs -I {} echo "$(date -d @$(date +\%s) +\%Y-\%m-\%d-\%H:\%M:\%S) {}" >> /home/ubuntu/.yacp-db
+    echo $(($(df -k /dev/sda1 | awk '{print $4}' | egrep "[0-9]") / 1000)) | xargs -I {} echo "$(date -d @$(date +\%s) +\%Y-\%m-\%d-\%H:\%M:\%S) {}" >> /home/ubuntu/.yacp-disk-available-mb
+    curl -s localhost:12798/metrics | grep "cardano_node_metrics_txsProcessedNum_int" | awk '{print $2}' | xargs -I {} echo "$(date -d @$(date +\%s) +\%Y-\%m-\%d-\%H:\%M:\%S) {}" >> /home/ubuntu/.yacp-tx
+    
     #send monitoring to telegram
-    # TODO - the problem ---> when epoch starts, everything is fresh so no monitoring to monitor
-    # /home/ubuntu/yacp/telegram/bot/f/_gnuplot.sh >> /tmp/plot
+    /home/ubuntu/yacp/telegram/bot/f/_gnuplot.sh >> /tmp/plot
+
+    rm /tmp/db_plot_image.png
+	  rm /home/ubuntu/.yacp-db
+    rm /tmp/disk_plot_image.png
+	  rm /home/ubuntu/.yacp-disk
+    rm /tmp/disk_available_plot_image.png
+	  rm /home/ubuntu/.yacp-disk-available-mb
+    rm /tmp/disk_available_plot_image.png
+	  rm /home/ubuntu/.yacp-tx
 
 fi
 
@@ -101,5 +118,5 @@ if [[ ! -z ${message} ]]; then
 fi
 
 if [[ "$(date +%H%M)" == "2300" ]]; then
-  rm /tmp/1min /tmp/5min /tmp/30min /tmp/5h /tmp/3h /tmp/far
+  rm /tmp/1min /tmp/5min /tmp/30min /tmp/5h /tmp/1h /tmp/far
 fi
